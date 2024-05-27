e90a6ed41ad613a5746d70ec53388fde49ccb701
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-05-27 18:23:10 +0100

    backup

--- find.js
+++ find.js
@@ -1,5 +1,27 @@
-function(context, args)  // target:#s.some.npc
+function(context, args)  // target:#s.script.name
 {
+	function mergeChar(oldArr, newArr) {
+		var flag = false;
+		for (var i = 0; i < oldArr.length; i++) {
+			if (oldArr[i] && oldArr[i].split('').some(char => !/^[a-zA-Z0-9_.<>]$/.test(char)) && oldArr[i] !== null) {
+				oldArr[i] = newArr[i];
+				flag = true;
+			}
+		}
+		return flag;
+	}
+	function get_log()
+	{
+		var out = [];
+		for (var j = 0; j < projects.length; j++)
+		{
+			l = args.target.call({[cmd_var]: cmd, [pass_key[i]]: pass, project: projects[j]});
+			if (Array.isArray(l))
+				out = out.concat(l);
+		}
+		return out;
+	}
+		
 	var l, words, get_pass, pass = null, get_user, match, cmd, cmd_var, i, regex = /^[a-zA-Z_:]+$/;
 	var pass_key = ["p", "pass", "password"];
 	var projects = ["W3rlD3NDER", "ESCHATOLOGI.EXE", "omegabyte_03", "qw_stop", "Ap_calypse", "BL4Z1NGW0RLD", "giant_spidr", "H0meEntert4inment", "thefloood", "Vy_for_russ", "tmp"];
@@ -19,14 +41,10 @@ function(context, args)  // target:#s.some.npc
 	} while (!regex.test(cmd) || !regex.test(cmd_var));
 
 	do {
-		var loop = 0;
 		l = args.target.call({[cmd_var]: get_pass});
 		match = l.match(/strategy (\S+)/);
 		if (!match)
-		{
-			loop++;
 			continue;
-		}
 		pass = match[1];
 	} while (!regex.test(pass) || pass === null);
 
@@ -38,11 +56,9 @@ function(context, args)  // target:#s.some.npc
 	}
 
 	var logs = [];
-	for (var j = 0; j < projects.length; j++)
-		{
-			l = args.target.call({[cmd_var]: cmd, [pass_key[i]]: pass, project: projects[j]});
-			if (Array.isArray(l))
-				logs = logs.concat(l);
-		}
-	return { ok: true, msg: logs};	
+	var new_logs = get_log();
+	do {
+		logs = get_log();
+	} while (mergeChar(new_logs, logs));
+	return { ok: true, msg: new_logs};	
 }
