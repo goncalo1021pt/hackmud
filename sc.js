0d0914365d7934d43109c0bf86e21d26262dfae3
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-05-30 22:18:10 +0100

    found magnara

--- sc.js
+++ sc.js
@@ -1,13 +1,34 @@
 function(context, args)
 {
-	var a = [];
 	var l;
-	for (var ctd = 0; ctd < 50; ctd++){
-		l = #ns.beta.lock_sim({locks:["sn_w_glock"]});
-		l = #ns.beta.lock_sim( {sn_w_glock:""});
-		if (a.includes(l))
-			continue
-		a.push(l);
+	var t;
+	l = #ns.beta.lock_sim( {magnara:""}).split(" ");
+	t = l[0];
+	l = l[l.length-1];
+	function gc(s){
+		var r = [];
+		if (s.length === 1) {
+			return [s];
+		} else {
+			var a = gc(s.slice(1));
+			for (var i = 0; i < a.length; i++) {
+				for (var j = 0; j < s.length; j++) {
+					var p = a[i].slice(0, j) + s[0] + a[i].slice(j);
+					if (r.indexOf(p) === -1)
+						r.push(p);
+				}
+			}
+			return r;
+		}
 	}
-	return {ok:true, a:a};
+	var c = gc(l);
+	var b;
+	for (var i = 0; i < c.length; i++) {
+		l = #ns.beta.lock_sim({magnara:c[i]});
+		b = l.split(" ");
+		b = b[0]; 
+		if (b !== t)
+			return {ok:true, c:c[i], l:l};
+	}
+	return {ok:true, c:c};
 }
